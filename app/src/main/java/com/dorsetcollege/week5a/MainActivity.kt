89a888012b6a99fdc3ddcebfed0eabd306ea54fe
John Rowley <outsidethebox@eircom.net>
2020-10-15 15:34:54 +0100

    First Commit Message

---
+++ app/src/main/java/com/dorsetcollege/week5a/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dorsetcollege.week5a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
